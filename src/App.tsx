b59dd72d5762dfcdc43a796259307bd12142da90
siliconeidolon <musasolarin@gmail.com>
2021-07-27 22:42:53 +0100

    feat: basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './App.css'
 
-import  { ReactElement } from 'react'
+import  { ReactElement, CSSProperties } from 'react'
 
-import { data as sizes } from './data'
+import { data } from './data'
 
 function App(): ReactElement {
 
   return (
-    <div className="App">
-      <pre>{JSON.stringify(sizes, null, 2)}</pre>
-      {sizes[0].sizes.map(size => {
+    <div className="app">
+      {data[0].sizes.map(size => {
         return (
           <Sheet key={size.name} paperSize={size} />
         )
@@ -34,7 +33,25 @@ export interface PaperFormat {
 
 const Sheet = ({ paperSize }: { paperSize: PaperSize}) => {
   const { name, mm, points, inches } = paperSize
+
+  const width = points[0]
+  const height = points[1]
+
+  const style: CSSProperties = {
+    width,
+    height,
+    maxWidth: width,
+    maxHeight: height,
+    minWidth: width,
+    minHeight: height
+  }
+
   return (
-    <div style={{ width: points[0], height: points[1], background: 'beige' }}>{name}</div>
+    <div className="sheet" style={style}>
+      <span>{name}</span>
+      <p>mm: {`${mm[0]} x ${mm[1]}`}</p>
+      <p>in: {`${inches[0]} x ${inches[1]}`}</p>
+      <p>pts: {`${points[0]} x ${points[1]}`}</p>
+    </div>
   )
 }
